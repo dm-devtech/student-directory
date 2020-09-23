f17ff0dfc6eb48b9365bfe1db406a40e80b264fb
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-09-23 11:38:51 +0000

    Fifteenth commit: adding interactive menu (step 9)

--- directory.rb
+++ directory.rb
@@ -1,21 +1,55 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students and their cohort"
+  puts "To finish, do not enter a name or other details - just press return to skip"
   #create an empty array
   students = []
   #get the first name
-  name = gets.chomp
+  name = gets.strip
+  puts "enter a cohort"
+  cohort = gets.strip.to_sym
+  while true do
+    if name.empty? 
+    puts "you must enter a name"
+    name = gets.strip
+    elsif cohort.empty?
+    puts "you must enter a cohort"
+    cohort = gets.strip.intern
+    else break
+    end
+  end
+  puts "enter hobby"
+  hobby = gets.strip
+  puts "enter birth country"
+  countryofbirth = gets.strip
+  puts "enter height"
+  height = gets.strip
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
-    name = gets.chomp
-  end 
-  #return the array of students
+    students << {name: name, cohort: cohort, hobby: hobby,
+    countryofbirth: countryofbirth, height: height}
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else 
+      puts "Now we have #{students.count} students"
+    end
+    puts "enter a new student: (press return to skip)"
+    name = gets.strip
+    if !name.empty? 
+      puts "enter a cohort"
+      cohort = gets.strip.to_sym
+      puts "enter hobby"
+      hobby = gets.strip
+      puts "enter birth country"
+      countryofbirth = gets.strip
+      puts "enter height"
+      height = gets.strip
+    else
+    end
+  end
+    #return the array of students
   students
-end
+end 
 
 def print_header
   puts "The students of Villains Academy"
@@ -23,16 +57,55 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    num = index + 1 
-    puts "#{num}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end 
-end
+  counter = 1
+  new_students = students.group_by {|x| x[:cohort]}
+  cohorts = []
+  students.map { |s| s[:cohort] }.uniq.each { |c| cohorts << c }
+  newerstudents = []
+  new_students.each {|k,v| newerstudents << v}
+  while counter <= students.length do
+    newerstudents.flatten.select do |v| 
+      puts counter.to_s+"."+ " name: "+v[:name].to_s.split(" ")[0].center(24)
+      puts "cohort: "+v[:cohort].to_s.center(30)
+      puts "hobby: "+v[:hobby].to_s.center(32)
+      puts "country of birth: "+v[:countryofbirth].to_s.center(4)
+      puts "height: "+v[:height].to_s.center(26)
+      counter += 1
+    end  
+  end
+end 
+
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  if names.count == 1 
+    puts "Overall, we have #{names.count} great student"
+  else names.count > 1
+    puts "Overall, we have #{names.count} great students"
+  end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    #1 print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # we'll be adding more lines
+    #2 read the input and save it into a variable
+    selection = gets.chomp
+    #3 do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
