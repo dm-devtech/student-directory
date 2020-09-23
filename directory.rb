340f561469963a6264da7c08136b6bcd81875126
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-09-23 12:34:11 +0000

    Seventeenth commit: added save to file functionality and gitignore file

--- directory.rb
+++ directory.rb
@@ -97,6 +97,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -112,11 +113,25 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
       puts "I don't know what you mean, try again"
   end
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w") #have to open the file first
+  #iterate over the students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]] #on every iteration creates array with name and cohort
+    csv_line = student_data.join(",") #convert to comma separated string and joins all elements from the string
+    file.puts csv_line #writes data to file
+  end
+  file.close
 end
 
 interactive_menu
