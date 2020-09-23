959c9765b8f0f3c4c072b905112403dd4b175f21
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-09-23 15:04:24 +0000

    22nd Commit: adding interaction messages

--- directory.rb
+++ directory.rb
@@ -112,14 +112,19 @@ end
 def process(selection)
   case selection
     when "1"
+      puts "Ok, moving to input students"
       input_students
     when "2"
+      puts "Ok, showing students"
       show_students
     when "3"
+      puts "Saving..."
       save_students
     when "4"
+      puts "Loading..."
       load_students
     when "9"
+      puts "Exiting"
       exit
     else
       puts "I don't know what you mean, try again"
