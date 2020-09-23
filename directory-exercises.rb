080f76de190cc69eab6b9c26537d9c1b7a849e84
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-09-23 10:35:22 +0000

    Thirteenth commit. Step 8 exercises: Grouping output by cohort.

--- directory-exercises.rb
+++ directory-exercises.rb
@@ -1,37 +1,55 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students and their cohort"
   puts "To finish, do not enter a name or other details - just press return to skip"
   #create an empty array
   students = []
   #get the first name
-  name = gets.chomp
+  name = gets.strip
+  puts "enter a cohort"
+  cohort = gets.strip.to_sym
+  while true do
+    if name.empty? 
+    puts "you must enter a name"
+    name = gets.strip
+    elsif cohort.empty?
+    puts "you must enter a cohort"
+    cohort = gets.strip.intern
+    else break
+    end
+  end
   puts "enter hobby"
-  hobby = gets.chomp
+  hobby = gets.strip
   puts "enter birth country"
-  countryofbirth = gets.chomp
+  countryofbirth = gets.strip
   puts "enter height"
-  height = gets.chomp
+  height = gets.strip
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november, hobby: hobby, 
+    students << {name: name, cohort: cohort, hobby: hobby,
     countryofbirth: countryofbirth, height: height}
-    puts "Now we have #{students.count} students"
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else 
+      puts "Now we have #{students.count} students"
+    end
     puts "enter a new student: (press return to skip)"
-    name = gets.chomp
+    name = gets.strip
     if !name.empty? 
+      puts "enter a cohort"
+      cohort = gets.strip.to_sym
       puts "enter hobby"
-      hobby = gets.chomp
+      hobby = gets.strip
       puts "enter birth country"
-      countryofbirth = gets.chomp
+      countryofbirth = gets.strip
       puts "enter height"
-      height = gets.chomp
+      height = gets.strip
     else
     end
-  end 
-  #return the array of students
+  end
+    #return the array of students
   students
-end
+end 
 
 def print_header
   puts "The students of Villains Academy"
@@ -40,20 +58,29 @@ end
 
 def print(students)
   counter = 1
-    while counter <= students.length do
-        students.select do |v| 
-          puts counter.to_s+". "+v[:name]
-          puts "cohort: "+v[:cohort].to_s
-          puts "hobby: "+v[:hobby].to_s
-          puts "country of birth: "+v[:countryofbirth].to_s
-          puts "height: "+v[:height].to_s
-          counter += 1
-        end  
-    end 
-end
+  new_students = students.group_by {|x| x[:cohort]}
+  cohorts = []
+  students.map { |s| s[:cohort] }.uniq.each { |c| cohorts << c }
+  newerstudents = []
+  new_students.each {|k,v| newerstudents << v}
+  while counter <= students.length do
+    newerstudents.flatten.select do |v| 
+      puts counter.to_s+"."+ " name: "+v[:name].to_s.split(" ")[0].center(24)
+      puts "cohort: "+v[:cohort].to_s.center(30)
+      puts "hobby: "+v[:hobby].to_s.center(32)
+      puts "country of birth: "+v[:countryofbirth].to_s.center(4)
+      puts "height: "+v[:height].to_s.center(26)
+      counter += 1
+    end  
+  end
+end 
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  if names.count == 1 
+    puts "Overall, we have #{names.count} great student"
+  else names.count > 1
+    puts "Overall, we have #{names.count} great students"
+  end
 end
 
 students = input_students
